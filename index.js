a1254ab415591fd84a5c3970776ba3bdcf2ff8b8
christophernathaniel <61963586+christophernathaniel@users.noreply.github.com>
2022-09-15 13:41:02 +0100

    Commit

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 //   process.env.NODE_ENV === "production" ||
 //   process.env.NODE_ENV === "staging"
 // ) {
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("/", (request, response) => {
+app.get(["/", "/login", "/register", "/dashboard"], (request, response) => {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 //}
